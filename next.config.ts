8524e12a17f164f9a8219447a3dab406ce063f0d
Sidharth 'R3FluX' Sreejil <94698713+sid-js@users.noreply.github.com>
2025-03-24 12:46:29 +0530

    feat: Remove webpack confi

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config, { dev, isServer, webpack, nextRuntime }) => {
-    config.module.rules.push({
-        test: /\.node$/,
-        loader: 'node-loader'
-    });
-    return config;
-}
+  
 };
 
 export default nextConfig;
