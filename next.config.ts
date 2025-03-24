f05e10258f2c44071ed5c6670f587faaf84c98b7
Sidharth 'R3FluX' Sreejil <94698713+sid-js@users.noreply.github.com>
2025-03-24 12:26:52 +0530

    feat: Update next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { dev, isServer, webpack, nextRuntime }) => {
+    config.module.rules.push({
+        test: /\.node$/,
+        loader: 'node-loader'
+    });
+    return config;
+}
 };
 
 export default nextConfig;
